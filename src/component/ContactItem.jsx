cc6fabb96cd4b5966cec849532510e42e63587ac
Shorecut <chermanderco@gmail.com>
2023-07-04 17:52:06 +0600

    ok

--- src/component/ContactItem.jsx
+++ src/component/ContactItem.jsx
@@ -2,24 +2,37 @@ import React from "react";
 import DeleteIcon from "../assets/delete.png";
 const ContactItem = ({ item, deleteContact, changeStatus }) => (
   <div className={`contact ${item.premium ? "premium" : ""}`}>
-    <input
+    {/*<input
       className="check_inp"
       onChange={() => {
         changeStatus(item.id);
       }}
       type="checkbox"
       checked={item.premium}
+    />*/}
+    <input
+      onChange={() => {
+        changeStatus(item.id);
+      }}
+      checked={item.premium}
+      type="checkbox"
+      id="star"
     />
+    <label for="star">
+      <svg viewBox="0 0 24 24">
+        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+      </svg>
+    </label>
 
-    <div className="list_right">name: {item.name} </div>
-    <div className="list_right">surname: {item.surname} </div>
-    <div className="list_right">phone: {item.number} </div>
+    <div className="list_right"> {item.name} </div>
+    <div className="list_right">{item.surname} </div>
+    <div className="list_right">{item.number} </div>
     <img
       onClick={(e) => {
         deleteContact(item.id);
       }}
-      style={{ marginLeft: "5px", marginTop: "4px", cursor: "pointer" }}
-      width={17}
+      style={{ marginLeft: "50px", marginTop: "3px", cursor: "pointer" }}
+      width={20}
       src={DeleteIcon}
       alt="delete"
     />
